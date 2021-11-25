19f7178db0854580ac8e1fb83c4c238f89c55f63
ryokosuge <ryo.kosuge@gmail.com>
2021-11-25 14:31:00 +0900

    :+1:	Styling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,32 @@
+import type { LinksFunction } from "remix";
+import { Link } from "remix";
+import stylesUrl from "../styles/index.css";
+
+export let links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: stylesUrl },
+  ];
+};
+
 const IndexRoute = () => {
   return (
-    <div>Hello Index Rotue</div>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">
+                Read Jokes
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
-}
+};
 
 export default IndexRoute;
